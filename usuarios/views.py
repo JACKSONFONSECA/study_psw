dc856e3d3b1fb0cd47d9f6b02d8d6d0589363a0a
jacksonfonseca <fonsecajackson@hotmail.com>
2024-01-16 20:17:23 -0300

    tela cadastro

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def cadastro(request):
-    return HttpResponse ('Olá Mundo!!!')
 
-# if request.method == 'GET':
-# return render(request, 'cadastro.html')
+def cadastro(request):
+    # if request.method == 'GET':
+    return render(request, 'cadastro.html')
